f1dbc2c6060f011508c53ded4ef346309a46c45c
ihenthorn <44933136+ihenthorn@users.noreply.github.com>
2018-11-18 17:37:46 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/db18", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/db", { useNewUrlParser: true });
 
 module.exports = mongoose;
